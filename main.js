b78d5e47bdee05eb4e25863346ef7d1d074fcea5
channhi07 <160860653+channhi07@users.noreply.github.com>
2024-02-29 21:04:29 +0700

    ynhii

---
+++ main.js
@@ -0,0 +1,78 @@
+var i=1;
+while (i<0) {
+
+}
+
+do {
+    console.log (i);
+    i++;
+
+} while (1<=10){
+    console.log(i)
+}
+
+var i=1;
+for(let i=1; i<=10; i++) {
+    console.log(i);
+}
+
+const nameStudent = [
+    "Yến Nhi",
+    "Chần Nhi",
+    "Bí nhee",
+
+]
+console.log(nameStudent);
+console.log(nameStudent[3]);
+console.log(nameStudent[3]);
+console.log(nameStudent[3]);
+
+var i=0;
+while (i <=nameStudent .leghth) {
+    console.log (nameStudent[i])
+    i++;
+}
+
+do{
+    console.log(nameStudent[1]);
+    i++;
+} while (i<nameStudent.lenghth);
+
+const classes = new Array (
+    "ABC",
+    ["jgng","jttj"],// mảng con ở vị trí thứ nhất 
+    "jHjb",
+    "dbb",
+)
+
+//console.log(classes.pop()); // in ra pth pop
+//console.log(classes); // mảng sau khi chưa thực thi pth pop
+//console.log(classes); // mảng khi chưa thực thi pth pop 
+
+//console.log("độ dài của mảng này là "+ classes.lenghth);
+//console.log(classes[2][1]);
+
+//console.log(classes);
+//delete classes[1];
+//console.log (classes);
+
+;
+const informations = {
+    name: "Bí Nhe", // name gọi laf Key,"bn" gọi là value
+    class:"JSA",
+    language:["hàn","anh"],
+    
+        certificate1 : "Toeic",
+        certificate2 : "Ielts",
+    
+ age: "16",
+}
+console.log(informations);
+console.log (informations.language[1].certificate2);
+
+
+//console.log(informations);
+//console.log(informations.name );
+
+//console.log(informations);
+//console.log(informations.lauguage[2]);
